f26a9ee31f97bb82111599cbbee288d4f53de1c6
Matthew Do <qvs82v2vm@mozmail.com>
2025-09-21 08:18:49 -0400

    renamed sup to Bound.sup

--- RealAnalysis/Bound/Basic.lean
+++ RealAnalysis/Bound/Basic.lean
@@ -8,7 +8,7 @@ example : BoundedAbove {x : ℚ | x * x < 2} (mkRat 3 2) := by
   intros x h_x_mul_x_lt_2
   sorry
 
-def sup {t} (subset : Set t) [LT t] [LE t] :=
+def Bound.sup {t} (subset : Set t) [LT t] [LE t] :=
   ∃ ub : t, ∀ x, x ∈ subset → x < ub → ¬BoundedAbove subset x
 
 def BoundedBelow {t} (subset : Set t) (lowerBound : t) [LE t] := ∀ x, x ∈ subset → lowerBound ≤ x
