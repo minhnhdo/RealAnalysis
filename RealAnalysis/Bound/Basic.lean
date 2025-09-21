a0afd94ec86ae9fe73c4bd3dd9f58181bf0bc694
Matthew Do <qvs82v2vm@mozmail.com>
2025-09-21 08:29:33 -0400

    added Bound.inf

--- RealAnalysis/Bound/Basic.lean
+++ RealAnalysis/Bound/Basic.lean
@@ -1,14 +1,12 @@
-import Mathlib.Data.Set.Basic
 import Mathlib.Data.Rat.Init
+import Mathlib.Data.Set.Basic
 
 def BoundedAbove {t} (subset : Set t) (upperBound : t) [LE t] := ∀ x, x ∈ subset → x ≤ upperBound
-
-example : BoundedAbove {x : ℚ | x * x < 2} (mkRat 3 2) := by
-  simp [BoundedAbove]
-  intros x h_x_mul_x_lt_2
-  sorry
 
 def Bound.sup {t} (subset : Set t) [LT t] [LE t] :=
   ∃ ub : t, ∀ x, x ∈ subset → x < ub → ¬BoundedAbove subset x
 
 def BoundedBelow {t} (subset : Set t) (lowerBound : t) [LE t] := ∀ x, x ∈ subset → lowerBound ≤ x
+
+def Bound.inf {t} (subset : Set t) [LT t] [LE t] :=
+  ∃ lb : t, ∀ x, x ∈ subset → x > lb → ¬BoundedBelow subset x
