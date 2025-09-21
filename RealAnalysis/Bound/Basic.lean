7d12e2fd5f8787d6b1665ffc7f4b20fd67181f44
Matthew Do <qvs82v2vm@mozmail.com>
2025-09-21 15:47:14 -0400

    added example

--- RealAnalysis/Bound/Basic.lean
+++ RealAnalysis/Bound/Basic.lean
@@ -1,12 +1,24 @@
+import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Rat.Init
 import Mathlib.Data.Set.Basic
 
-def BoundedAbove {t} (subset : Set t) (upperBound : t) [LE t] := ∀ x, x ∈ subset → x ≤ upperBound
+def BoundedAbove {t} [LE t] (subset : Set t) (upperBound : t) := ∀ x, x ∈ subset → x ≤ upperBound
 
-def Bound.sup {t} (subset : Set t) [LT t] [LE t] :=
-  ∃ ub : t, ∀ x, x ∈ subset → x < ub → ¬BoundedAbove subset x
+def Bound.sup {t} [LT t] [LE t] (subset : Set t) :=
+  ∃ ub, BoundedAbove subset ub → ∀ x, x ∈ subset → x < ub → ¬BoundedAbove subset x
 
-def BoundedBelow {t} (subset : Set t) (lowerBound : t) [LE t] := ∀ x, x ∈ subset → lowerBound ≤ x
+def BoundedBelow {t} [LE t] (subset : Set t) (lowerBound : t) := ∀ x, x ∈ subset → lowerBound ≤ x
 
-def Bound.inf {t} (subset : Set t) [LT t] [LE t] :=
-  ∃ lb : t, ∀ x, x ∈ subset → x > lb → ¬BoundedBelow subset x
+def Bound.inf {t} [LT t] [LE t] (subset : Set t) :=
+  ∃ lb, BoundedBelow subset lb → ∀ x, x ∈ subset → lb < x → ¬BoundedBelow subset x
+
+example : Bound.sup (Finset.cons 2
+                      ((Finset.cons 1
+                        (Finset.cons (mkRat 1 2) Finset.empty (by decide))
+                        (by decide)))
+                      (by decide)).toSet := by
+  use 2
+  intros _ _ _ h_x_lt_lub h_x_is_ub
+  simp [BoundedAbove] at *
+  apply Rat.not_lt.mpr h_x_is_ub.left
+  exact h_x_lt_lub
