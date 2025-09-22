d734d24fa0bf1210ec79cd8c07e14c08f3f3bcdf
Matthew Do <qvs82v2vm@mozmail.com>
2025-09-22 15:49:19 -0400

    renamed variables

--- RealAnalysis/Bound/Basic.lean
+++ RealAnalysis/Bound/Basic.lean
@@ -2,15 +2,13 @@ import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Rat.Init
 import Mathlib.Data.Set.Basic
 
-def Set.BoundedAbove (subset : Set ℚ) (upperBound : ℚ) := ∀ x, x ∈ subset → x ≤ upperBound
+def Set.BoundedAbove (s : Set ℚ) (upperBound : ℚ) := ∀ x, x ∈ s → x ≤ upperBound
 
-def Set.Sup (subset : Set ℚ) (lub : ℚ) :=
-  subset.BoundedAbove lub ∧ ∀ ub, subset.BoundedAbove ub → lub ≤ ub
+def Set.Sup (s : Set ℚ) (lub : ℚ) := s.BoundedAbove lub ∧ ∀ ub, s.BoundedAbove ub → lub ≤ ub
 
-def Set.BoundedBelow (subset : Set ℚ) (lowerBound : ℚ) := ∀ x, x ∈ subset → lowerBound ≤ x
+def Set.BoundedBelow (s : Set ℚ) (lowerBound : ℚ) := ∀ x, x ∈ s → lowerBound ≤ x
 
-def Set.Inf (subset : Set ℚ) (glb : ℚ) :=
-  subset.BoundedBelow glb ∧ ∀ lb, subset.BoundedBelow lb → glb ≤ lb
+def Set.Inf (s : Set ℚ) (glb : ℚ) := s.BoundedBelow glb ∧ ∀ lb, s.BoundedBelow lb → glb ≤ lb
 
 example : (Finset.cons 2
             ((Finset.cons 1
--- RealAnalysis/Bound/Lemmas.lean
+++ RealAnalysis/Bound/Lemmas.lean
@@ -4,62 +4,62 @@ import Mathlib.Data.Set.Basic
 import RealAnalysis.Bound.Basic
 
 theorem Set.BoundedAbove_trans
-  (subset : Set ℚ) (ub₁ ub₂ : ℚ)
-  (h_ub₁ : subset.BoundedAbove ub₁)
+  (s : Set ℚ) (ub₁ ub₂ : ℚ)
+  (h_ub₁ : s.BoundedAbove ub₁)
   (h_ub₁_le_ub₂ : ub₁ ≤ ub₂)
-  : subset.BoundedAbove ub₂ := by
+  : s.BoundedAbove ub₂ := by
     simp [Set.BoundedAbove] at *
-    intros x h_x_in_subset
-    have h_x_le_ub₁ := h_ub₁ x h_x_in_subset
+    intros x h_x_in_s
+    have h_x_le_ub₁ := h_ub₁ x h_x_in_s
     apply le_trans h_x_le_ub₁
     assumption
 
 theorem Set.Sup_le_ub_iff
-  (subset : Set ℚ) (lub ub : ℚ)
-  (h_lub : subset.Sup lub)
-  :  lub ≤ ub ↔ subset.BoundedAbove ub := by
+  (s : Set ℚ) (lub ub : ℚ)
+  (h_lub : s.Sup lub)
+  :  lub ≤ ub ↔ s.BoundedAbove ub := by
     apply Iff.intro
-    · exact subset.BoundedAbove_trans lub ub h_lub.left
+    · exact s.BoundedAbove_trans lub ub h_lub.left
     · intro h_ub
       simp [Set.Sup] at *
       exact h_lub.right ub h_ub
 
 theorem Set.Sup_le_bound_iff
-  (subset : Set ℚ) (lub b : ℚ) (h_lub : subset.Sup lub)
-  : (∀ x, x ∈ subset → x ≤ b) ↔ lub ≤ b := by
+  (s : Set ℚ) (lub b : ℚ) (h_lub : s.Sup lub)
+  : (∀ x, x ∈ s → x ≤ b) ↔ lub ≤ b := by
     simp [Set.Sup] at *
     apply Iff.intro
     · intro h_b
       apply h_lub.right
       assumption
     · intros h_lub_le_b
-      apply subset.BoundedAbove_trans lub b
+      apply s.BoundedAbove_trans lub b
       · exact h_lub.left
       · assumption
 
 theorem Set.lt_BoundedAbove
-  (subset : Set ℚ) (b : ℚ)
-  : (∀ x, x ∈ subset → x < b) → subset.BoundedAbove b := by
-    intros h_lt x h_x_in_subset
+  (s : Set ℚ) (b : ℚ)
+  : (∀ x, x ∈ s → x < b) → s.BoundedAbove b := by
+    intros h_lt x h_x_in_s
     apply Rat.le_of_lt
-    exact h_lt x h_x_in_subset
+    exact h_lt x h_x_in_s
 
 theorem Set.lt_Sup_le
-  (subset : Set ℚ) (lub b : ℚ) (h_lub : subset.Sup lub)
-  : (∀ x, x ∈ subset → x < b) → lub ≤ b := by
+  (s : Set ℚ) (lub b : ℚ) (h_lub : s.Sup lub)
+  : (∀ x, x ∈ s → x < b) → lub ≤ b := by
     intro h_lt
     apply h_lub.right
-    exact subset.lt_BoundedAbove b h_lt
+    exact s.lt_BoundedAbove b h_lt
 
 theorem Set.lt_Sup
-  (subset : Set ℚ) (b lub : ℚ) (h_lub : subset.Sup lub) (h_lt : b < lub)
-  : ¬subset.BoundedAbove b := by
+  (s : Set ℚ) (b lub : ℚ) (h_lub : s.Sup lub) (h_lt : b < lub)
+  : ¬s.BoundedAbove b := by
     intro h_ub
     have h_lub_le_b := h_lub.right b h_ub
     have h_not_lub_le_b := LT.lt.not_ge h_lt
     contradiction
 
-theorem Set.subset_imp_Sup_le
+theorem Set.s_imp_Sup_le
   (s₁ s₂ : Set ℚ) (lub₁ lub₂ : ℚ)
   (h_lub₁ : s₁.Sup lub₁) (h_lub₂ : s₂.Sup lub₂) (h_subset : s₁ ⊆ s₂)
   : lub₁ ≤ lub₂ := by
