a7bf0c1a1dac0072837e6ae1c192bd45dc1cba5b
Matthew Do <qvs82v2vm@mozmail.com>
2025-09-22 08:20:30 -0400

    fixed def

--- RealAnalysis/Bound/Basic.lean
+++ RealAnalysis/Bound/Basic.lean
@@ -4,20 +4,19 @@ import Mathlib.Data.Set.Basic
 
 def BoundedAbove {t} [LE t] (subset : Set t) (upperBound : t) := ∀ x, x ∈ subset → x ≤ upperBound
 
-def Bound.Sup {t} [LT t] [LE t] (subset : Set t) :=
-  ∃ ub, BoundedAbove subset ub → ∀ x, x < ub → ¬BoundedAbove subset x
+def Bound.Sup {t} [LT t] [LE t] (subset : Set t) (ub : t) :=
+  BoundedAbove subset ub → ∀ x, x < ub → ¬BoundedAbove subset x
 
 def BoundedBelow {t} [LE t] (subset : Set t) (lowerBound : t) := ∀ x, x ∈ subset → lowerBound ≤ x
 
-def Bound.Inf {t} [LT t] [LE t] (subset : Set t) :=
-  ∃ lb, BoundedBelow subset lb → ∀ x, lb < x → ¬BoundedBelow subset x
+def Bound.Inf {t} [LT t] [LE t] (subset : Set t) (lb : t) :=
+  BoundedBelow subset lb → ∀ x, lb < x → ¬BoundedBelow subset x
 
 example : Bound.Sup (Finset.cons 2
                       ((Finset.cons 1
                         (Finset.cons (mkRat 1 2) Finset.empty (by decide))
                         (by decide)))
-                      (by decide)).toSet := by
-  use 2
+                      (by decide)).toSet 2 := by
   intros _ _ h_x_lt_lub h_x_is_ub
   simp [BoundedAbove] at *
   have not_x_lt_lub := Rat.not_lt.mpr h_x_is_ub.left
