3bfcc2fe41d5f86e1400d46aa96832d75a656fcd
Matthew Do <qvs82v2vm@mozmail.com>
2025-09-22 09:29:04 -0400

    added BoundedAbove_trans

--- RealAnalysis/Bound/Basic.lean
+++ RealAnalysis/Bound/Basic.lean
@@ -1,15 +1,17 @@
+import Mathlib.Algebra.Order.Ring.Unbundled.Rat
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Rat.Init
 import Mathlib.Data.Set.Basic
+import Mathlib.Order.Defs.PartialOrder
 
-def BoundedAbove {t} [LE t] (subset : Set t) (upperBound : t) := ∀ x, x ∈ subset → x ≤ upperBound
+def BoundedAbove (subset : Set ℚ) (upperBound : ℚ) := ∀ x, x ∈ subset → x ≤ upperBound
 
-def Bound.Sup {t} [LT t] [LE t] (subset : Set t) (lub : t) :=
+def Bound.Sup (subset : Set ℚ) (lub : ℚ) :=
   BoundedAbove subset lub ∧ ∀ x, x < lub → ¬BoundedAbove subset x
 
-def BoundedBelow {t} [LE t] (subset : Set t) (lowerBound : t) := ∀ x, x ∈ subset → lowerBound ≤ x
+def BoundedBelow (subset : Set ℚ) (lowerBound : ℚ) := ∀ x, x ∈ subset → lowerBound ≤ x
 
-def Bound.Inf {t} [LT t] [LE t] (subset : Set t) (lb : t) :=
+def Bound.Inf (subset : Set ℚ) (lb : ℚ) :=
   BoundedBelow subset lb ∧ ∀ x, lb < x → ¬BoundedBelow subset x
 
 example : Bound.Sup (Finset.cons 2
@@ -25,3 +27,14 @@ example : Bound.Sup (Finset.cons 2
     simp [BoundedAbove] at *
     have not_x_lt_lub := Rat.not_lt.mpr h_x_is_ub.left
     contradiction
+
+theorem BoundedAbove_trans
+  (subset : Set ℚ) (ub₁ : ℚ) (ub₂ : ℚ)
+  (h_ub₁ : BoundedAbove subset ub₁)
+  (h_ub₁_le_ub₂ : ub₁ ≤ ub₂)
+  : BoundedAbove subset ub₂ := by
+    simp [BoundedAbove] at *
+    intros x h_x_in_subset
+    have h_x_le_ub₁ := h_ub₁ x h_x_in_subset
+    apply le_trans h_x_le_ub₁
+    assumption
