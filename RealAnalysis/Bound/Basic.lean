1982cfa45f40f72b1379142557c3d7290a3625c9
Matthew Do <qvs82v2vm@mozmail.com>
2025-09-22 09:35:27 -0400

    added Bound.lub_le_ub_iff

--- RealAnalysis/Bound/Basic.lean
+++ RealAnalysis/Bound/Basic.lean
@@ -2,7 +2,6 @@ import Mathlib.Algebra.Order.Ring.Unbundled.Rat
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Rat.Init
 import Mathlib.Data.Set.Basic
-import Mathlib.Order.Defs.PartialOrder
 
 def BoundedAbove (subset : Set ℚ) (upperBound : ℚ) := ∀ x, x ∈ subset → x ≤ upperBound
 
@@ -38,3 +37,13 @@ theorem BoundedAbove_trans
     have h_x_le_ub₁ := h_ub₁ x h_x_in_subset
     apply le_trans h_x_le_ub₁
     assumption
+
+theorem Bound.lub_le_ub_iff
+  (subset : Set ℚ) (lub : ℚ) (ub : ℚ)
+  (h_lub : Bound.Sup subset lub)
+  : BoundedAbove subset ub ↔ lub ≤ ub := by
+    simp [Bound.Sup] at *
+    apply Iff.intro
+    · sorry
+    · intro h_lub_le_ub
+      exact BoundedAbove_trans subset lub ub h_lub.left h_lub_le_ub
