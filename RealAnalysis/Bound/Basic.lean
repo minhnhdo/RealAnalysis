5710f6295867e9b2809f2afcacc1be2cd161ea85
Matthew Do <qvs82v2vm@mozmail.com>
2025-09-22 09:04:23 -0400

    fixed types

--- RealAnalysis/Bound/Basic.lean
+++ RealAnalysis/Bound/Basic.lean
@@ -2,22 +2,26 @@ import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Rat.Init
 import Mathlib.Data.Set.Basic
 
-def BoundedAbove (subset : Set ℚ) (upperBound : ℚ) := ∀ x, x ∈ subset → x ≤ upperBound
+def BoundedAbove {t} [LE t] (subset : Set t) (upperBound : t) := ∀ x, x ∈ subset → x ≤ upperBound
 
-def Bound.Sup (subset : Set ℚ) (ub : ℚ) :=
-  BoundedAbove subset ub → ∀ x, x < ub → ¬BoundedAbove subset x
+def Bound.Sup {t} [LT t] [LE t] (subset : Set t) (lub : t) :=
+  BoundedAbove subset lub ∧ ∀ x, x < lub → ¬BoundedAbove subset x
 
-def BoundedBelow (subset : Set ℚ) (lowerBound : ℚ) := ∀ x, x ∈ subset → lowerBound ≤ x
+def BoundedBelow {t} [LE t] (subset : Set t) (lowerBound : t) := ∀ x, x ∈ subset → lowerBound ≤ x
 
-def Bound.Inf (subset : Set ℚ) (lb : ℚ) :=
-  BoundedBelow subset lb → ∀ x, lb < x → ¬BoundedBelow subset x
+def Bound.Inf {t} [LT t] [LE t] (subset : Set t) (lb : t) :=
+  BoundedBelow subset lb ∧ ∀ x, lb < x → ¬BoundedBelow subset x
 
 example : Bound.Sup (Finset.cons 2
                       ((Finset.cons 1
                         (Finset.cons (mkRat 1 2) Finset.empty (by decide))
                         (by decide)))
                       (by decide)).toSet 2 := by
-  intros _ _ h_x_lt_lub h_x_is_ub
-  simp [BoundedAbove] at *
-  have not_x_lt_lub := Rat.not_lt.mpr h_x_is_ub.left
-  contradiction
+  simp [Bound.Sup] at *
+  apply And.intro
+  · simp [BoundedAbove] at *
+    decide
+  · intros x h_x_lt_lub h_x_is_ub
+    simp [BoundedAbove] at *
+    have not_x_lt_lub := Rat.not_lt.mpr h_x_is_ub.left
+    contradiction
