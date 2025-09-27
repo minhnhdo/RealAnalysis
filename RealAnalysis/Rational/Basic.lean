3a469735fb3fdc07118ec44d9876a3c4257dd3cc
Matthew Do <qvs82v2vm@mozmail.com>
2025-09-26 22:50:06 -0400

    reorg

--- RealAnalysis/Rational/Basic.lean
+++ RealAnalysis/Rational/Basic.lean
@@ -1,59 +1,16 @@
-structure Rational where
-  numerator : Int
-  denominator : Nat
-  denominator_ne_zero : denominator ≠ 0
+import RealAnalysis.Rational.Pre.Basic
+import RealAnalysis.Rational.Pre.Lemmas
 
-instance : Inhabited Rational where
-  default := ⟨0, 1, by decide⟩
+def Rational := Quotient (PreRational.instSetoid)
 
-instance {n} : OfNat Rational n where
-  ofNat := ⟨n, 1, by decide⟩
+def Rational.addAux (p q : PreRational) : Rational := Quotient.mk' (PreRational.add p q)
 
-protected def Rational.neg (q : Rational) : Rational :=
-  ⟨-q.numerator, q.denominator, q.denominator_ne_zero⟩
+theorem Rational.addAux_lift
+  (p q r s : PreRational)
+  (h_pr_equiv : p ≈ r)
+  (h_qs_equiv : q ≈ s)
+  : Rational.addAux p q = Rational.addAux r s := by
+    apply Quotient.sound
+    exact PreRational.add_well_defined p q r s h_pr_equiv h_qs_equiv
 
-instance : Neg Rational where
-  neg := Rational.neg
-
-instance : ToString Rational where
-  toString q := s!"{q.numerator}/{q.denominator}"
-
-@[simp]
-protected def Rational.equivalent_relation (p q : Rational) : Prop :=
-  p.numerator * q.denominator = p.denominator * q.numerator
-  deriving Decidable
-
-instance : HasEquiv Rational := ⟨Rational.equivalent_relation⟩
-
-protected def Rational.addNumerator (p q : Rational) : Int :=
-  p.numerator * q.denominator + p.denominator * q.numerator
-
-protected def Rational.addDenominator (p q : Rational) : Nat :=
-  p.denominator * q.denominator
-
-protected def Rational.add (p q : Rational) : Rational :=
-  let prf : p.denominator * q.denominator ≠ 0 := by
-    apply Nat.mul_ne_zero
-    exact p.denominator_ne_zero
-    exact q.denominator_ne_zero
-  ⟨p.addNumerator q, p.addDenominator q, prf⟩
-
-instance : Add Rational where
-  add := Rational.add
-
-protected def Rational.sub (p q : Rational) : Rational :=
-  p + (-q)
-
-instance : Sub Rational where
-  sub := Rational.sub
-
-def Rational.isPositive (p : Rational) : Prop :=
-  0 < p.numerator * p.denominator
-  deriving Decidable
-
-protected def Rational.lt : Rational → Rational → Prop
-  | p, q => (q - p).isPositive
-  deriving Decidable
-
-instance : LT Rational where
-  lt := Rational.lt
+def Rational.add (p q : Rational) : Rational := Quotient.lift₂ Rational.addAux Rational.addAux_lift p q
