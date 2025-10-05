ee0b1d5517fd11c70eacd97b5144d369cf38412e
Matthew Do <qvs82v2vm@mozmail.com>
2025-10-05 00:17:16 -0400

    finished proof

--- RealAnalysis/Int/Lemmas.lean
+++ RealAnalysis/Int/Lemmas.lean
@@ -1,3 +1,22 @@
 import Mathlib.Data.Int.Basic
+import Mathlib.Tactic
 
-theorem Int.neg_eq_self {a : ℤ} : -a = a ↔ a = 0 := by sorry
+theorem Int.neg_eq_self {a : ℤ} : -a = a ↔ a = 0 := by
+  constructor
+  · intro h
+    have h' := calc 0
+      _ = a + a := by
+        rw [← Int.add_left_neg a, Int.add_left_inj a]
+        assumption
+      _ = a * 2 := by
+        rw [← Int.add_right_inj (-a), ← Int.add_assoc, Int.add_left_neg]
+        rw [Int.zero_add, Int.add_comm, Int.add_neg_eq_sub]
+        nth_rw 3 [← Int.mul_one a]
+        rw [← Int.mul_sub a]
+        simp
+    symm at h'
+    rw [Int.mul_eq_zero] at h'
+    simp at h'
+    assumption
+  · intro
+    simp [*]
