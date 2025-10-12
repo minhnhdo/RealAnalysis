fe781ebc28fcc2a38f85be33033a8a9c0f5d4ae0
Matthew Do <qvs82v2vm@mozmail.com>
2025-10-11 22:01:56 -0400

    added example

--- RealAnalysis/Set/Metric.lean
+++ RealAnalysis/Set/Metric.lean
@@ -29,12 +29,35 @@ namespace Metric
 
 variable {t : Type u} [MetricSpace t]
 
+theorem dist_nonneg {p q : t} : 0 ≤ dist p q := MetricSpace.dist_nonneg p q
+
+@[simp]
+theorem dist_eq_zero {p q : t} : dist p q = 0 ↔ p = q := MetricSpace.dist_eq_zero p q
+
+theorem dist_triangle {p q r : t} : dist p r ≤ dist p q + dist q r :=
+  MetricSpace.dist_triangle p q r
+
 def openBall (x : t) (r : ℝ) : Set t := { y : t | dist x y < r }
 
+@[simp]
 alias ball := openBall
 
 def closedBall (x : t) (r : ℝ) : Set t := { y : t | dist x y ≤ r }
 
 def IsLimitPoint (p : t) (s : Set t) := ∀ r, ∃ q, q ∈ ball p r ∧ q ≠ p → q ∈ s
+
+theorem pos_of_mem_ball {r : ℝ} {p q : t} (h : q ∈ ball p r) : 0 < r := by
+  simp [openBall] at *
+  apply lt_of_le_of_lt (MetricSpace.dist_nonneg p q)
+  assumption
+
+example : IsLimitPoint 0 { p : ℝ | ∃ n : ℕ, (p = (n + (1 : ℝ))⁻¹ ∨ p = -(n + (1 : ℝ))⁻¹) } := by
+  simp [IsLimitPoint, openBall, dist]
+  intro r
+  use (1 + (1 : ℝ))⁻¹
+  intro hr hne
+  use 1
+  left
+  simp
 
 end Metric
